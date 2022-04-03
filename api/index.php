30c94b9af164fb30f5d32b7208efffa3c808eac8
SaikatGeek <53232305+SaikatGeek@users.noreply.github.com>
2022-04-03 21:08:21 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -12,21 +12,3 @@ use Api\Controller\ImageController;
 Router::get("mock", "ImageController@resizeImage");
 
 
-// print_r( new Http);
-
-// new ImageController;
-
-// var_dump( $_POST['data'] );
-// print_r( json_decode( file_get_contents('php://input'), true )->data );
-
-
-$dir = new \DirectoryIterator( realpath( "E:\Javascript\Kent C. Dodds - Testing JavaScript" ) );
-
-
-
-foreach ($dir as $fileInfo) {
-    if( $fileName = str_replace("TutFlix.io--", " F-", $fileInfo->getFilename() ) ) {
-
-        rename("E:\Javascript\Kent C. Dodds - Testing JavaScript\\". $fileInfo->getFilename(), "E:\Javascript\Kent C. Dodds - Testing JavaScript\\" . $fileName);
-    }
-}
