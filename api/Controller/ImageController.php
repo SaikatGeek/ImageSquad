60361d2d8090e7614833925e223a1199f898ff41
SaikatGeek <saikat.geek@gmail.com>
2022-04-06 00:21:15 +0600

    ImageController - ImagickBlob to base64 encode return

--- api/Controller/ImageController.php
+++ api/Controller/ImageController.php
@@ -13,21 +13,21 @@ class ImageController extends FileHandler
     public static function resizeImage()
     {
         $file = new FileHandler;
-        $array = json_decode( file_get_contents('php://input'), true ) ;
-        var_dump("ss");
+        // $array = json_decode( file_get_contents('php://input'), true ) ;
+        // var_dump("ss");
 
         // header('Content-Type: application/json; charset=utf-8');
-        echo json_encode( ["array" => [1, 88, "kk"]] );
+        // echo json_encode( ["array" => [1, 88, "kk"]] );
 
         
-        // self::$width = 500;
-        // self::$height = 450;
-        // $imagick = new \Imagick( realpath($file->projectRootPath()."\api\Storage\RawImage\\1.jpg") );
-        // $imagick->adaptiveResizeImage(self::$width, self::$height, $bestFit = false);
-        // header("Content-Type: image/jpeg");
-        // // echo $imagick->getImageBlob();
+        self::$width = 500;
+        self::$height = 450;
+        $imagick = new \Imagick( realpath($file->projectRootPath()."\api\Storage\RawImage\\1.jpg") );
+        $imagick->adaptiveResizeImage(self::$width, self::$height, $bestFit = false);
+        header("Content-Type: image/jpeg");
+        echo base64_encode($imagick->getImageBlob());
         // $imagick->writeImage ('ff.jpg');
-        // $imagick->clear();
+        $imagick->clear();
     }
 
     
