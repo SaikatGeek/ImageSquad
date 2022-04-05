bcae1e4d08d19f17908e24a2602066cb3d313622
SaikatGeek <saikat.geek@gmail.com>
2022-04-05 16:40:40 +0600

    index.php modified

--- api/index.php
+++ api/index.php
@@ -10,16 +10,3 @@ use Api\Controller\ImageController;
 
 // echo Request::endpoint();
 Router::get("mock", "ImageController@resizeImage");
-
-
-<<<<<<< HEAD
-// print_r( new Http);
-
-// new ImageController;
-
-// var_dump( $_POST['data'] );
-// print_r( json_decode( file_get_contents('php://input'), true )->data );
-
-
-=======
->>>>>>> 30c94b9af164fb30f5d32b7208efffa3c808eac8
