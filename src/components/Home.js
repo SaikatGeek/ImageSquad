fbe11abb366b1144eee22fa4dcb4302563206d87
SaikatGeek <saikat.geek@gmail.com>
2022-04-05 17:30:34 +0600

    .gitignore modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Home = () => {
+    const bb = () => {
+        return "punh";
+    }
+
+
     return (
         <div className="Container">
             <div className="row">
                 <div className="col">
-                    Home Page
+                    {bb()}
                 </div>
             </div>
         </div>
