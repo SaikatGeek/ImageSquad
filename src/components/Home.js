80e87192a11392aba407eb86c1a7a1c6aca3b861
SaikatGeek <saikat.geek@gmail.com>
2022-04-06 17:07:41 +0600

    Client Side Api Method Modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,9 +10,13 @@ const Home = () => {
     useEffect( () => {
         let url = `${env.API_BASE_URL}/mock`;
         (async () => {
-            await axios.get(url)
+            await axios.post(url, {
+                width: 560,
+                height: 450
+            })
             .then( response => {
-                setImg(response.data);
+                // setImg(response.data);
+                console.log(response);
             })
             .catch( error => {
                 console.log(error);
@@ -26,7 +30,7 @@ const Home = () => {
         <div className="Container">
             <div className="row">
                 <div className="col">
-                    <img src={`data:image/jpeg;base64,${img}`} />
+                    {/* <img src={`data:image/jpeg;base64,${img}`} /> */}
                 </div>
             </div>
         </div>
