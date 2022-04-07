a226c0e154e702b1394f85ada7863b6a228be373
SaikatGeek <saikat.geek@gmail.com>
2022-04-07 17:03:40 +0600

    Modified route controller

--- api/Controller/ImageController.php
+++ api/Controller/ImageController.php
@@ -4,11 +4,29 @@ namespace Api\Controller;
 
 use Api\Helper\FileHandler;
 
-class ImageController extends FileHandler
+class ImageController 
 {
     public static $imageFolderPath;
     public static $width;
     public static $height;
+
+    public static function galleryImageList()
+    {
+        $galleryImageList = [];
+        $iterator = new \DirectoryIterator( realpath($_SERVER["DOCUMENT_ROOT"]."/api/Storage/RawImage") );
+        
+        foreach ($iterator as $fileInfo) {
+            if( $iterator->isFile() ){
+                if(in_array( $iterator->getExtension(), ['jpg', 'png', 'jpeg'])){
+                    array_push($galleryImageList, "/api/Storage/RawImage/" . $iterator->getFilename() );
+                }
+            }
+        }
+
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($galleryImageList);
+
+    }
     
     public static function resizeImage()
     {
@@ -17,9 +35,9 @@ class ImageController extends FileHandler
         self::$height = 450;
         $imagick = new \Imagick( realpath($file->projectRootPath()."\api\Storage\RawImage\\1.jpg") );
         $imagick->adaptiveResizeImage(self::$width, self::$height, $bestFit = false);
-        header("Content-Type: image/jpeg");
-        echo base64_encode($imagick->getImageBlob());
-       
+        // header("Content-Type: image/jpeg");
+        // echo $imagick->getImageBlob();
+        echo "img";
         $imagick->clear();
     }
 
