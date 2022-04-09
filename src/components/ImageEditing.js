3d509f878efddb513b4e636f83c9bedf6cf44a0d
SaikatGeek <saikat.geek@gmail.com>
2022-04-09 23:32:31 +0600

    ImageEditing Data protected

--- src/components/ImageEditing.js
+++ src/components/ImageEditing.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const ImageEditing = () => {
+  const location = useLocation();
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    if(location === undefined || location.state === null){
+      navigation(-1);
+    }
+    console.log(location)
+  }, []);
+
   return (
     <div>ImageEditing</div>
   )
