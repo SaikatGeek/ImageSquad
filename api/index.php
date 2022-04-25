9b655b27d0e51a3d7de22a341380908abd09c8c6
SaikatGeek <saikat.geek@gmail.com>
2022-04-26 00:59:07 +0600

    Backend index file modified

--- api/index.php
+++ api/index.php
@@ -2,13 +2,7 @@
 error_reporting(0);
 include "./autoload.php";
 
-use Api\Helper\Request;
 use Api\Helper\Router;
-use Api\Helper\Http;
-use Api\Controller\ImageController;
-
-
-// echo Request::endpoint();
 
 Router::get("gallery", "ImageController@galleryImageList");
 Router::get("resize", "ImageController@resizeImage");
@@ -17,6 +11,5 @@ Router::post("sharpen", "ImageController@sharpenImage");
 Router::post("wave", "ImageController@waveImage");
 Router::post("contrast", "ImageController@localContrastImage");
 Router::post("annotate", "ImageController@annotateImage");
-
 
 
