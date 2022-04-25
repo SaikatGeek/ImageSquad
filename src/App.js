adb1b1693f8b0fd804026704a7de75947c04225f
SaikatGeek <saikat.geek@gmail.com>
2022-04-26 01:09:44 +0600

    Space reduced

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.min.js'; 
 import Header from './components/Header';
 import Home from './components/Home';
 import ImageEditing from './components/ImageEditing';
